b9c4bfda3677b80094e0df2e61f7856bc2bcfd45
Alexis Mercier des Rochettes <32903418+alexismdr@users.noreply.github.com>
2024-08-09 20:25:41 +0200

    Compatibility update for PixelBuilds Unity

--- key-migration/export-keys-test.sh
+++ key-migration/export-keys-test.sh
@@ -15,7 +15,7 @@ for item in \
     "network:networkstack" \
     "nfc:nfc" \
     "platform:platform" \
-    "release:releasekey" \
+    "test:testkey" \
     "sdk:sdk_sandbox" \
     "shared:shared" \
     "verifiedboot:verifiedboot"
@@ -23,11 +23,14 @@ do
     prefix="${item%%:*}"
     filename="${item#*:}"
 
-    if [ "$prefix" = "release" ]; then
+    if [ "$prefix" = "test" ]; then
         echo ${prefix}_cert=\"$(openssl x509 -outform der -in $filename.x509.pem | xxd -p  | tr -d '\n')\"
         echo ${prefix}_key=\"$(openssl x509 -pubkey -noout -in $filename.x509.pem | grep -v '-' | tr -d '\n')\"
+    elif [[ "$prefix" =~ ^(cyngn|verifiedboot)$ ]]; then
+        echo \# ${prefix}_cert_test=\"\"\(UNUSED\)
+        echo \# ${prefix}_key_test=\"\" \(UNUSED\)
     else
-        echo ${prefix}_cert_release=\"$(openssl x509 -outform der -in $filename.x509.pem | xxd -p  | tr -d '\n')\"
-        echo ${prefix}_key_release=\"$(openssl x509 -pubkey -noout -in $filename.x509.pem | grep -v '-' | tr -d '\n')\"
+        echo ${prefix}_cert_test=\"$(openssl x509 -outform der -in $filename.x509.pem | xxd -p  | tr -d '\n')\"
+        echo ${prefix}_key_test=\"$(openssl x509 -pubkey -noout -in $filename.x509.pem | grep -v '-' | tr -d '\n')\"
     fi 
 done
